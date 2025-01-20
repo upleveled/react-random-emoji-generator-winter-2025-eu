6bb3c5796df1d4d3254729fb7dde7a51dbb02449
Lukas Prochazka <prochazka.lu@gmail.com>
2025-01-20 13:08:30 +0100

    Add random emoji generator

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # React Emoji Generator
+
+React app that generates random emojis and displays them on the screen. The user can also search for a specific emoji by typing in the input field.
+
+- random emoji on page load
+- new random emoji on Generate button click
+- show emoji name under emoji
+- choosing an emoji by name
+- showing error if invalid emoji name entered
+
+## TODO
+
+- [x] random emoji maybe with library
+- [x] useState with initial value (random emoji)
+- [x] create a button
+- [x] generate new emoji on button click
+- [x] add emoji name under emoji
+- [x] create input field, connect to state
+- [x] show error message when emoji doesn't exist
--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import './App.css';
-import logo from './logo.svg';
+import { get, has, random } from 'node-emoji';
+import { useState } from 'react';
 
 export default function App() {
+  // { name: 'house', emoji: '🏠' }
+  const initialEmoji = random();
+  // 1. Create a state for the input
+  const [emoji, setEmoji] = useState(initialEmoji.emoji);
+  const [emojiInput, setEmojiInput] = useState(initialEmoji.name);
+
+  const hasEmoji = has(emojiInput);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        fontFamily: 'sans-serif',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Random Emoji Generator</h1>
+      <div
+        style={{
+          height: '180px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {hasEmoji && (
+          <>
+            <div style={{ fontSize: '100px' }}>{emoji}</div>
+            <div
+              style={{
+                backgroundColor: '#f0f0f0',
+                padding: '5px 10px',
+                marginBottom: '16px',
+                borderRadius: '16px',
+              }}
+            >
+              {emojiInput}
+            </div>
+          </>
+        )}
+      </div>
+      <div>
+        <input
+          // 2. Set the value to the input
+          value={emojiInput}
+          onChange={(event) => {
+            // 3. Set Emoji name to input value
+            setEmojiInput(event.currentTarget.value);
+            // get Emoji from the input value
+            const foundEmoji = get(event.currentTarget.value);
+            // set Emoji icon to found emoji
+            setEmoji(foundEmoji);
+          }}
+        />
+        <button
+          onClick={() => {
+            const newEmoji = random();
+            setEmoji(newEmoji.emoji);
+            setEmojiInput(newEmoji.name);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Generate Random
+        </button>
+      </div>
+      {!hasEmoji && (
+        <div
+          style={{
+            backgroundColor: '#f7b7b7',
+            padding: '5px 10px',
+            marginBottom: '16px',
+            borderRadius: '16px',
+          }}
+        >
+          This emoji doesn't exist
+        </div>
+      )}
     </div>
   );
 }
